bed79fe23816d40d7ceb43e7a12eef8eccca2bbd
karan2527 <karanjain2527@gmail.com>
2025-01-05 22:02:29 +0530

    Mobile UI compeleted

---
+++ lib/info.dart
@@ -0,0 +1,121 @@
+const info = [
+  {
+    'name': 'Karan Boii(You)',
+    'message': 'List = began,aaloo,pyaaz,tamatar',
+    'time': '3:53 pm',
+    'profilePic':
+        'https://i.pinimg.com/736x/1b/fc/e9/1bfce97a85aecdd0c0a0cd48348c15ef.jpg',
+  },
+  {
+    'name': 'Priya GF',
+    'message': 'Hello, Aaj free ho?',
+    'time': '2:25 pm',
+    'profilePic':
+        'https://i.pinimg.com/736x/f8/f8/27/f8f82737bd5b0df12ae2e9c313187a5e.jpg',
+  },
+  {
+    'name': 'vasooli bhai',
+    'message': 'Aajj ka hafta? or 150 baki',
+    'time': '12:06 pm',
+    'profilePic':
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0UnCs54Yy9pgSDqV1O3nkjYOVVumbM7DqdXzFSFk9knYLxqxUWdw6hc-oMx9wYKSDTQI',
+  },
+  {
+    'name': 'Priya ki mummy',
+    'message': 'Hello dear..shaam ko free ho? ',
+    'time': '1:03 pm',
+    'profilePic':
+        'https://i.pinimg.com/736x/9d/74/40/9d7440de6fc939aa0387bc0bed8b9122.jpg',
+  },
+  {
+    'name': 'majnu bhai',
+    'message': 'Meri behan se shaadi karlee',
+    'time': '10:00 am',
+    'profilePic': 'https://c.ndtvimg.com/gws/4548/assets/6.jpeg?1640334077',
+  },
+  {
+    'name': 'Ranga',
+    'message': 'Bhailog,raat ka kya seen h?',
+    'time': '7:25 am',
+    'profilePic':
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSA7LD2C8XN9foGpFw14AmsGeIUCeZi0ECbt9V1GfAcN0bsBrUGZiMxiPd6D2xI3zFhhzw',
+  },
+  {
+    'name': 'Moona darling ',
+    'message': 'kya ree chiknee',
+    'time': '9:53 am',
+    'profilePic':
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQScgPSD9a2lVaFycy8szo18C8Iz0zH2o65tA&s',
+  },
+  {
+    'name': 'Circuit ',
+    'message': 'Munna bhai bulla ree ley tere ko',
+    'time': '6:02 am',
+    'profilePic':
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZvbTeM7ZnjrrP-jKjiTISXLOQ11vbSudZCQ&s',
+  },
+  {
+    'name': 'Sikandar',
+    'message': 'Lets Code!',
+    'time': '4:56 am',
+    'profilePic':
+        'https://images.unsplash.com/photo-1619194617062-5a61b9c6a049?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHJhbmRvbSUyMHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
+  },
+  {
+    'name': 'Ian Dooley',
+    'message': 'Images by Unsplash',
+    'time': '1:00 am',
+    'profilePic':
+        'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+  },
+];
+
+const messages = [
+  {"isMe": false, "text": "Hey What is up with you!!", "time": "10:00 am"},
+  {"isMe": true, "text": "im fine,wbu?", "time": "11:00 am"},
+  {"isMe": false, "text": "I am great man!", "time": "11:01 am"},
+  {
+    "isMe": false,
+    "text": "Just messaged cuz I had some work.",
+    "time": "11:01 am"
+  },
+  {"isMe": true, "text": "Obviously, say", "time": "11:03 am"},
+  {
+    "isMe": false,
+    "text": "haha I wanted you to check out my new channel ^^",
+    "time": "11:04 am"
+  },
+  {
+    "isMe": true,
+    "text": " Sure, what is the channel name?",
+    "time": "11:05 am"
+  },
+  {
+    "isMe": false,
+    "text": "Rivaan Ranawat",
+    "time": "11:06 am",
+  },
+  {
+    "isMe": true,
+    "text": "Looks great to me!",
+    "time": "11:15 am",
+  },
+  {"isMe": false, "text": "Thanks bro!", "time": "11:17 am"},
+  {"isMe": false, "text": "Did you subscribe?", "time": "11:16 am"},
+  {"isMe": true, "text": "Yes, surely bro!", "time": "11:17 am"},
+  {
+    "isMe": false,
+    "text": "Cool, did you like the content?",
+    "time": "11:18 am",
+  },
+  {
+    "isMe": true,
+    "text": "I loved it?",
+    "time": "11:19 am",
+  },
+  {
+    "isMe": false,
+    "text": "OMG! Woah! Thanks!",
+    "time": "11:20 am",
+  },
+];
