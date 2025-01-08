51b01b9b29f24fbde7fba65ac11c8f54e736b93c
KARAN JAIN <167758524+karan2527@users.noreply.github.com>
2025-01-08 22:37:46 +0530

    web profile

--- lib/widgets/web_profile_bar.dart
+++ lib/widgets/web_profile_bar.dart
@@ -1,43 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/colors.dart';
 
-class ChatAppBar extends StatelessWidget {
-  const ChatAppBar({Key? key}) : super(key: key);
+class WebProfileBar extends StatelessWidget {
+  const WebProfileBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height * 0.077,
-      width: MediaQuery.of(context).size.width * 0.75,
+      width: MediaQuery.of(context).size.width * 0.25,
       padding: const EdgeInsets.all(10.0),
-      color: webAppBarColor,
+      decoration: const BoxDecoration(
+        border: Border(
+          right: BorderSide(
+            color: dividerColor,
+          ),
+        ),
+        color: webAppBarColor,
+      ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Row(
-            children: [
-              const CircleAvatar(
-                backgroundImage: NetworkImage(
-                  'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
-                ),
-                radius: 20,
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.01,
-              ),
-              const Text(
-                'Rivaan Ranawat',
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-            ],
+          const CircleAvatar(
+            backgroundImage: NetworkImage(
+              'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+            ),
+            radius: 20,
           ),
           Row(
             children: [
               IconButton(
                 onPressed: () {},
-                icon: const Icon(Icons.search, color: Colors.grey),
+                icon: const Icon(Icons.comment, color: Colors.grey),
               ),
               IconButton(
                 onPressed: () {},
